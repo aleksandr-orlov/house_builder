60dac5f5db3e5c1340f4afda65b45ad978b7c2ef
Aleksandr Orlov <aleksandr-m-orlov@yandex.ru>
2014-12-29 14:51:05 +0400

    Сделан шаблон проекта.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by aleksandr on 29.12.14.
+ */
+
+var hb = {};
+
+hb.init = function() {
+    if (localStorage.hbPage) {
+        if (localStorage.hbPage == 1) {
+
+        } else if (localStorage.hbPage == 2) {
+
+        } else if (localStorage.hbPage == 3) {
+
+        }
+    } else {
+        localStorage.hbPage = 1;
+    }
+};
+
+hb.materials = {};
+hb.typeFoundation = {};
+hb.typeRoof = {};
+hb.construction = {};
